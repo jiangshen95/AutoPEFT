d027e5ff636d00e6a727b951dffaf97d9544331e
wusland <1306170882@qq.com>
2024-04-25 12:00:09 +0800

    update src

--- src/peft_search_space.py
+++ src/peft_search_space.py
@@ -1,5 +1,51 @@
+'''A config class that can be used to define the type, location, 
+and number of PEFTs
+This config input is a args parser. The output is a PEFTSearchSpace object
+main.py will only use the PEFTSearchSpace object to generate the model structure
+'''
+
+MAX_MODEL_LAYER = 12
+
+
 class PEFTSearchSpace:
     '''
-    使用该类来定义PEFT的类型，位置和数量
+    PEFTSearchSpace is a class that defines the search space of PEFTs
     '''
-    pass
+    configs = None
+    args = None
+
+    def __init__(self, args):
+        '''
+        args: a argparse.ArgumentParser object
+        '''
+        self.args = args
+        self.configs = {}
+        if args.base_lora:
+            self.configs['lora'] = {
+                'type': 'lora',
+                'ranks': [args.base_lora for _ in range(MAX_MODEL_LAYER)]
+            }
+        if args.base_adapter:
+            self.configs['adapter'] = {
+                'type': 'adapter',
+                'bn': [args.base_adapter for _ in range(MAX_MODEL_LAYER)]
+            }
+        if args.lora:
+            self.configs['lora'] = {
+                'type': 'lora',
+                'ranks': args.lora,  # a list of ranks
+            }
+            self.configs['lora']['ranks'] = [
+                max(0, rank) for rank in self.configs['lora']['ranks']
+            ]
+        if args.adapter:
+            self.configs['adapter'] = {
+                'type': 'adapter',
+                'bn': args.adapter,  # a list of bottleneck sizes
+            }
+            self.configs['adapter']['bn'] = [
+                max(0, bn) for bn in self.configs['adapter']['bn']
+            ]
+
+    def get_config(self):
+        return self.configs
--- src/run_model.py
+++ src/run_model.py
@@ -1,101 +1,72 @@
-from adapters import AutoAdapterModel, AdapterArguments, AdapterTrainer, AdapterConfig, ConfigUnion, LoRAConfig, SeqBnConfig, PrefixTuningConfig
+'''The main function that runs the model
+It can be used by 2 ways:
+1. run 'python run_model.py --lora 64' in the terminal
+2. use PEFTModel.run(args/configs) in other python files
+'''
+
+import argparse
 import sys
-from torch.utils.data import DataLoader
-from datasets import load_dataset
-from adapters.trainer import AdapterTrainer
-from adapters.composition import Stack
-from adapters import AutoAdapterModel
-from transformers import RobertaTokenizer, TrainingArguments
-from sklearn.metrics import accuracy_score, precision_recall_fscore_support
-import numpy as np
+import os
 import torch
+import numpy as np
+from adapters import AutoAdapterModel, AdapterArguments, AdapterTrainer, AdapterConfig, ConfigUnion, LoRAConfig, SeqBnConfig, PrefixTuningConfig
+from transformers import RobertaTokenizer, TrainingArguments
+from src.peft_search_space import PEFTSearchSpace
 
 
-def print_trainable_parameters(model):
-    trainable_params = 0
-    all_param = 0
-    for _, param in model.named_parameters():
-        all_param += param.numel()
-        if param.requires_grad:
-            trainable_params += param.numel()
-    print(
-        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param:.2f}"
-    )
+class PEFTModel:
+    '''Model wrapper
+    '''
 
+    configs = None
+    model = None
+    tokenizer = None
+    trainer = None
+    dataset = None
+    model_name = 'roberta-base'
+    task_name = 'mytask'
 
-# 加载模型和分词器
-model = AutoAdapterModel.from_pretrained("roberta-base")
-model.add_classification_head('rotten_tomatoes', num_labels=2)
-tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
+    def __init__(self, configs, dataset=None):
+        '''
+        configs: a dict of configs
+        dataset: a dataset object
+        '''
+        self.configs = configs
+        self.dataset = dataset
+        self.model = AutoAdapterModel.from_pretrained(self.model_name)
+        self.tokenizer = RobertaTokenizer.from_pretrained(self.model_name)
 
-lora_config = LoRAConfig(
-    r=64,  # 设置LoRA的rank
-    alpha=32,  # LoRA的alpha值，决定参数增加的数量
-    dropout=0.1,  # LoRA层的dropout比例
-    # leave_out=[6, 7, 8, 9, 10, 11],  # 指定需要转换的层 #important
-)
-# 添加LoRA适配器
-model.add_adapter("lora", config=lora_config)
-# model.set_active_adapters("lora")
-model.train_adapter("lora")
+        # calculate the number of labels
+        num_labels = dataset["test"].features["label"].num_classes
+        self.model.add_classification_head(
+            self.task_name, num_labels=num_labels)
 
-print_trainable_parameters(model)
-
-# 加载数据集
-dataset = load_dataset('super_glue', 'axb')  # 请替换为你的数据集
-print(dataset)
-dataset = dataset['test'].train_test_split(test_size=0.5)
-# 对数据集进行预处理
-
-
-def preprocess_function(examples):
-    return tokenizer(examples["text"], truncation=True, padding="max_length")
-
-
-def preprocess_function1(examples):
-    return tokenizer(
-        examples["sentence1"], truncation=True, padding="max_length")
-
-
-def preprocess_function2(examples):
-    return tokenizer(
-        examples["sentence2"], truncation=True, padding="max_length")
-
-
-encoded_dataset_train = dataset['train'].map(preprocess_function1, batched=True)
-encoded_dataset_train = dataset['train'].map(preprocess_function1, batched=True)
-
-encoded_dataset_val = dataset['test'].map(preprocess_function2, batched=True)
-encoded_dataset_val = dataset['test'].map(preprocess_function2, batched=True)
-
-# 定义训练参数
-training_args = TrainingArguments(
-    output_dir="./results",
-    num_train_epochs=10,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=64,
-    warmup_steps=10,
-    weight_decay=0.01,
-    logging_dir="./logs",
-    evaluation_strategy="epoch",
-)
-
-
-def compute_metrics(pred):
-    labels = pred.label_ids
-    preds = pred.predictions.argmax(-1)
-    acc = np.sum(preds == labels) / len(labels)
-    return {'accuracy': acc}
-
-
-# 创建Trainer
-trainer = AdapterTrainer(
-    model=model,
-    args=training_args,
-    train_dataset=encoded_dataset_train,
-    eval_dataset=encoded_dataset_val,
-    compute_metrics=compute_metrics,
-)
-
-# 开始训练
-trainer.train()
+        lora_config = None
+        adapter_config = None
+        if configs.get('base_lora'):
+            lora_config = LoRAConfig(
+                r=next(x for x in configs['lora']['ranks'] if x != 0),
+                alpha=r,
+                dropout=0.1,
+            )
+        if configs.get('base_adapter'):
+            adapter_config = SeqBnConfig(
+                reduction_factor=next(
+                    x for x in configs['adapter']['bn'] if x != 0),)
+        if configs.get('lora'):
+            lora_config = LoRAConfig(
+                r=next(x for x in configs['lora']['ranks'] if x != 0),
+                alpha=r,
+                dropout=0.1,
+                leave_out=[
+                    i for i, x in enumerate(configs['lora']['ranks']) if x == 0
+                ],
+            )
+        if configs.get('adapter'):
+            adapter_config = SeqBnConfig(
+                reduction_factor=next(
+                    x for x in configs['adapter']['bn'] if x != 0),
+                leave_out=[
+                    i for i, x in enumerate(configs['adapter']['bn']) if x == 0
+                ],
+            )
